209695e1ede67abbdef8c8ac054ebc4c8833c098
Hermela29-20 <hermela2029@gmail.com>
2026-05-09 20:28:33 +0300

    Added translated text

---
+++ lib/core/localization/translated_text.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TranslatedText extends StatelessWidget {
+  final String text;
+  final TextStyle? style;
+  final TextAlign? textAlign;
+  final int? maxLines;
+  final TextOverflow? overflow;
+
+  const TranslatedText(
+    this.text, {
+    super.key,
+    this.style,
+    this.textAlign,
+    this.maxLines,
+    this.overflow,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: style,
+      textAlign: textAlign,
+      maxLines: maxLines,
+      overflow: overflow,
+    );
+  }
+}
