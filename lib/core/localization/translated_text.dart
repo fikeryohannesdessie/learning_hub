25e3aba9d45e91338682f6f35dcb2d6b792233f5
Hermela29-20 <hermela2029@gmail.com>
2026-05-23 19:11:19 +0300

    Updated translated text

--- lib/core/localization/translated_text.dart
+++ lib/core/localization/translated_text.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'app_translations.dart';
 
-class TranslatedText extends StatelessWidget {
+/// Renders [text] translated to the active language using Google Translate.
+/// Shows original text while translating, then swaps to translated version.
+class TranslatedText extends ConsumerStatefulWidget {
   final String text;
   final TextStyle? style;
   final TextAlign? textAlign;
@@ -17,13 +21,54 @@ class TranslatedText extends StatelessWidget {
   });
 
   @override
+  ConsumerState<TranslatedText> createState() => _TranslatedTextState();
+}
+
+class _TranslatedTextState extends ConsumerState<TranslatedText> {
+  String _displayText = '';
+  String _lastLang = 'en';
+  String _lastOriginal = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _displayText = widget.text;
+    _lastOriginal = widget.text;
+  }
+
+  void _doTranslate(String lang) {
+    _lastLang = lang;
+    _lastOriginal = widget.text;
+    translateText(widget.text, lang).then((translated) {
+      if (mounted &&
+          ref.read(languageProvider) == lang &&
+          widget.text == _lastOriginal) {
+        setState(() => _displayText = translated);
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final lang = ref.watch(languageProvider);
+
+    // Re-translate if language or text changed
+    if (lang != _lastLang || widget.text != _lastOriginal) {
+      if (lang == 'en') {
+        _displayText = widget.text;
+        _lastLang = lang;
+        _lastOriginal = widget.text;
+      } else {
+        WidgetsBinding.instance.addPostFrameCallback((_) => _doTranslate(lang));
+      }
+    }
+
     return Text(
-      text,
-      style: style,
-      textAlign: textAlign,
-      maxLines: maxLines,
-      overflow: overflow,
+      _displayText,
+      style: widget.style,
+      textAlign: widget.textAlign,
+      maxLines: widget.maxLines,
+      overflow: widget.overflow,
     );
   }
 }
